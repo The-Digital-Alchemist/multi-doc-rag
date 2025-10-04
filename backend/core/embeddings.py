391d425e00adceb27802e283fe7c2970a0422e37
murad <murad2000balushi@gmail.com>
2025-10-04 22:54:00 +0400

    feat: created function to embed chunks, downgraded openai lib to an older version

---
+++ backend/core/embeddings.py
@@ -0,0 +1,13 @@
+from openai import OpenAI
+import numpy as np
+
+client = OpenAI()
+
+def embed_chunks(chunks: list[str], model="text-embedding-3-small") -> list[np.ndarray]:
+    response = client.embeddings.create(
+        model=model,
+        input=chunks
+    )
+
+    embeddings = [np.array(item.embedding) for item in response.data]
+    return embeddings
