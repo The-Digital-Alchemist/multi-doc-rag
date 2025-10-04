ef534b16a61b117633c9d4f4f2ff3f0ecd058cc4
murad <murad2000balushi@gmail.com>
2025-10-04 22:12:26 +0400

    feat: added in a function to read text from txt, pdf and doxc files

--- backend/utils/io.py
+++ backend/utils/io.py
@@ -16,3 +16,36 @@ def save_upload(file_obj, dest_dir: str) -> str:
         f.write(file_obj.file.read())
     
     return fpath
+
+
+def read_text_from_path(path: str) -> str:
+    
+    ext = Path(path).suffix.lower()
+    # Extracting text from a txt file
+    if ext == ".txt":
+        with open(path, "r", encoding="UTF-8", errors="ignore") as f:
+            return f.read()
+
+    elif ext == ".pdf":
+        # Extracting text from a pdf file
+        chunks = []
+        reader = PdfReader(path)
+
+        for page in reader.pages:
+            t = page.extract_text() or ""
+            chunks.append(t)
+        return "\n".join(chunks).strip()
+
+    elif ext == ".docx":
+        # Extracting text from a docx file
+        chunks = []
+        doc = docx.Document(path)
+
+        for p in doc.paragraphs:
+            t = p.text or ""
+            chunks.append(t)
+        return "\n".join(chunks).strip()
+ 
+
+    else:
+        raise ValueError("Unsupported file type")
